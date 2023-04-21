151b5ea47586973f68307305a84fa40b74d8d3a4
Pugking4 <joshuas13006@gmail.com>
2023-04-22 03:46:56 +1000

    edit

--- release/frontend/ratingcal.js
+++ release/frontend/ratingcal.js
@@ -1,9 +1,9 @@
 // Add a click event listener to the Calculate button
 document.getElementById('calculate-btn').addEventListener('click', () => {
   // Get the values of the input fields
-  const constant = constantInput.value;
-  const achievement = achievementInput.value;
-  const rating = ratingInput.value;
+  const constant = document.getElementById("constant").value;
+  const achievement = document.getElementById("achievement").value;
+  const rating = document.getElementById("rating").value;
 
   // Send an AJAX request to the server with the input values
   const xhr = new XMLHttpRequest();
