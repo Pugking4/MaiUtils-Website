aa2d97f58e4ac06e116b600891ea490f42a80b62
Pugking4 <joshuas13006@gmail.com>
2023-04-22 04:17:45 +1000

    edit

--- release/backend/app.py
+++ release/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -16,8 +16,14 @@ def calculate():
 
     # Process the data here
 
-    header_text = "Calculation complete!"
-    return header_text
+    # Redirect the user to a new page with a success message
+    success_message = "Calculation complete!"
+    return redirect(url_for('result', message=success_message))
+
+@app.route('/result')
+def result():
+    message = request.args.get('message')
+    return render_template('result.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True, host='192.168.58.50', port=5000)
