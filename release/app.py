6b9fee46c8c9769bcd96db69e9b857e457bc771f
Pugking4 <joshuas13006@gmail.com>
2023-04-23 12:48:47 +1000

    edit

--- release/app.py
+++ release/app.py
@@ -70,4 +70,4 @@ def result():
     return render_template('ratingcal.html', constant=constant, achievement=achievement, rating=rating, s_rating=s_rating, splus_rating=splus_rating, ss_rating=ss_rating, ssplus_rating=ssplus_rating, sss_rating=sss_rating, sssplus_rating=sssplus_rating, custom_rating=custom_rating, header_text=header_text)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
