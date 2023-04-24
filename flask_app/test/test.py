7f0e1914b1374f4734557f218962adc48bc5fde0
Pugking4 <joshuas13006@gmail.com>
2023-04-24 22:32:32 +1000

    Update test.py

--- flask_app/test/test.py
+++ flask_app/test/test.py
@@ -1,22 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
-
-options = webdriver.ChromeOptions()
-#options.add_argument("--headless")
 options = Options()
 # browser is Chromium instead of Chrome
 options.BinaryLocation = "/usr/bin/chromium-browser"
 # we use custom chromedriver for raspberry
 driver_path = "/usr/bin/chromedriver"
-driver = webdriver.Chrome(service=Service(driver_path))
+driver = webdriver.Chrome(options=options, service=Service(driver_path))
 
-driver.get('https://www.google.com')
+driver.get("https://stackoverflow.com/")
 
 print("bruh")
-
-Service(driver_path).stop()
